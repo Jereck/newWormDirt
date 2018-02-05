a9f3d457f267a7ae56943a6501460209e0d220d2
Jake <jakereck@gmail.com>
2018-02-04 16:56:22 -0800

    initial commit

---
+++ public/lib/hero.js
@@ -0,0 +1,11 @@
+var options = {
+    strings: ["<h3>Specializing in Moble-Frist Development</h3>", "<h3>Bringing Your Business To the Front Page</h3>"],
+    typeSpeed: 40,
+    contentType: "html"
+}
+
+var typed = new Typed('#typed', options);
+
+function menuFunction(x) {
+    x.classList.toggle("change");
+}
---
+++ views/email.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['submit'])) {
+    $to = "wormdirt@wormdirtdevelopment.com";
+    $from = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
+    $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
+    $message .= 'Message: ' . $_POST['message'];
+
+    $headers = "From: " . $from;
+    $headers2 = "From: " . $to;
+    mail($to, $subject, $message, $headers);
+    mail($from, $subject, $message, $headers2);
+
+    echo "Mail sent!";
+}
+
+?>
