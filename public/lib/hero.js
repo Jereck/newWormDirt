6fe070d0ad9dde3f3c8990d09f3aab8130fba2b5
Jake <jakereck@gmail.com>
2018-02-04 17:03:28 -0800

    Changing the typing.js

--- public/lib/hero.js
+++ public/lib/hero.js
@@ -1,5 +1,5 @@
 var options = {
-    strings: ["<h3>Specializing in Moble-Frist Development</h3>", "<h3>Bringing Your Business To the Front Page</h3>"],
+    strings: ["<h3>For All Your Website Solutions</h3>", "<h3>Bringing Your Business To the Front Page</h3>"],
     typeSpeed: 40,
     contentType: "html"
 }
